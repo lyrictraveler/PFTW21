35164a2ab79dbef95fc3f75a6cf694a735f4e6a3
Welling Hall <whall@mcad.edu>
2021-01-26 22:54:28 -0500

    created first array

---
+++ wk2/assignments/moviearrays/moviearrays.js
@@ -0,0 +1,12 @@
+const title = window.prompt("What is your favorite movie?");
+console.log(title);
+
+const myArr = ['A Fish Called Wanda', 'Dr. Strangelove', 'Shakespeare In Love', 'Pride and Prejudice', 'Dick'];
+
+console.log(myArr);
+
+myArr.push(title);
+
+console.log(myArr.toString());
+
+const movies = window.alert(myArr);
