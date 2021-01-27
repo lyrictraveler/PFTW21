ec5f96fab7033452fea4c58ab82759709cb8f9c4
Welling Hall <whall@mcad.edu>
2021-01-27 14:50:43 -0500

    stuck between array and object

---
+++ wk2/assignments/test_object/experiment.js
@@ -0,0 +1,18 @@
+function Game(title, type, numberOfPlayers, rating, shortDescription) {
+    this.title = title;
+    this.type = type;
+    this.numberOfPlayers = players;
+    this.rating = rating;
+    this.shortDescription = description;
+    }
+    
+    var game1 = new Game ('Scrabble', 'board', '2 or more', '1', 'a wordy game for word nerds');
+    
+    var game2 = new Game ('baseball', 'ball','18', '2', 'the all-American pastime');
+    
+    var game3 = new Game ('Cooking Fever', 'video', '1', '3', 'how to cook without getting your kitchen messy');
+    
+    function displayGame() {
+      var result = 
+    }
+    
