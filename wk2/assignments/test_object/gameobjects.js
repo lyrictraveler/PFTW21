5e300693035e59667e10fbcc6ce9da888cde2eb7
Welling Hall <whall@mcad.edu>
2021-01-27 14:47:39 -0500

    Working on game array

---
+++ wk2/assignments/test_object/gameobjects.js
@@ -0,0 +1,38 @@
+const myGame1 = {
+  title: 'Scrabble',
+  type: 'board',
+  players: '2 or more',
+  rating: 1,
+  shortDescription: 'a wordy word game for nerds'
+}
+
+console.log(myGame1.type);
+
+const myGame2 = {
+  title: 'baseball',
+  type: 'ball',
+  players: '9 per team',
+  rating: 3,
+  shortDescription: 'the all-American pastime'
+}
+
+console.log(myGame2.players);
+
+const myGame3 = {
+  title: 'Cooking Fever',
+  type: 'video',
+  players: 'single player',
+  rating: 2,
+  shortDescription: 'visit many restaurants during a pandemic'
+}
+
+console.log(myGame3.shortDescription);
+
+const myGameArray = 
+[
+  'myGame1',
+  'myGame2',
+  'myGame3'
+];
+
+console.log(myGameArr);
