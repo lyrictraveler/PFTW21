5363895daeb3253eac797ce6b1ae006ba05e1c3c
Welling Hall <whall@mcad.edu>
2021-02-07 15:53:25 -0500

    instructions added to page

--- wk3/assignments/radial/radial.js
+++ wk3/assignments/radial/radial.js
@@ -23,7 +23,7 @@ function makeArm(rotateBy) {
   noFill();
   stroke(stroke1);
   strokeWeight(heft); // my made-up value worked!!!
-  textSize(300);
+  textSize(200);
   text(string, 10, 100 / alt); // word, x-coordinate, y-coordinate; it worked with the prompt!!!
 }
 
@@ -31,31 +31,18 @@ function makeArm(rotateBy) {
 
 function draw() {
   background("white");
-  translate(200, 200);
+  translate(300, 300);
   rotate(rotateBy);
   makeArm(rotateBy);
-  rotateBy += 2; // I *thought* this was controlling the speed of the rotation, but this still confuses me.
+  rotateBy += 2; // I thought this was controlling the speed of the rotation, but this still confuses me.
+}
 
 // Defining what the mouse does
 
 function mousePressed() {
-  noLoop();
-}
-
-function mouseReleased() {
   loop();
 }
 
-/* Create a function to draw one ‘arm’ of your radial design and then use `rotate` to rotate your canvas a specified amount and then redraw your arm. You will find the most interesting designs will change after a certain amount of applications (maybe after a full rotation…?). Using parameters, alter the ‘arm’ and then continue the process. And by all means, don’t think you have to only use a small portion of the canvas for each iteration. Experiment and see what looks best.
-
-Must have:
-Must be a radial design generated from multiple applications of a function.
-Must be parameterized with at least three parameters
-Must only ‘draw’ within your function. Nothing should be drawn outside of your repeated function.
-Should have:
-Should be error-free. I should not see any errors in the dev console.
-Should be clean, well commented code.
-Should have appropriately named parameters.
-Nice to have:
-Ability to interact with sketch as it processes.
-Ability to stop the draw loop. */
+function mouseReleased() {
+  noLoop();
+}
