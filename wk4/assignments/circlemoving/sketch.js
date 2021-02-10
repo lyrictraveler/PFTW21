f56761f32a5ad2b869a28edc340e5ebc263e070d
Welling Hall <whall@mcad.edu>
2021-02-10 10:47:35 -0500

    This is buggy. Don't know how to make it work.

---
+++ wk4/assignments/circlemoving/sketch.js
@@ -0,0 +1,50 @@
+// Setting up condition for movement across canvas
+
+
+let x; 
+let y; 
+let xspeed; 
+let yspeed; 
+
+function setup() { 
+  createCanvas(1000, 1000);
+  x = width/2
+  y = height/2
+  xspeed = -1; 
+  yspeed = 0.5*xspeed; 
+}
+
+function draw() {                              // 1 = draw function
+  background('white');
+  ellipse(x, y, 75); 
+  x+=xspeed; 
+  y+=yspeed; 
+  
+  // Draw a circle
+  stroke('violet');
+  strokeWeight(5);
+  fill('aqua');
+  //ellipse(x, y, 25, 25);
+  
+ var d = dist(mouseX, mouseY, x, y)
+  
+
+ // not the right condition, but what is??? how detect ellipse moving outside of canvas? 
+    if(rect > width) {   
+        // announce the count
+    noLoop();
+    textSize(28);
+    textStyle(BOLDITALIC);
+    text('Your score was ' + clickCount + '!', 100, 300);
+}
+  
+  // click the mouse
+  function mousePressed () {       
+    
+    // 3 = if condition, but why aren't clicks being detected
+  if (d < radius) {                            
+      clickCount++;
+    console.log('hit', clickCount);
+  }
+}
+}
---
+++ wk4/assignments/domdemo/wk4_exercise_2/wk4_2.js
@@ -0,0 +1,46 @@
+// definition of variables and constants
+
+let rectX = 0;
+const rectHeight = 150;
+let rectY;
+const rectWidth = 150;
+let clickCount = 0;
+let speed;
+
+// setup here creates the canvas and randomizes the starting position of the rectangle on the y axis and randomizes speed (although I'm not sure what the speed ranges are)
+
+function setup () {
+  createCanvas(1000, 1000);
+  rectY = random(height - rectHeight);
+  speed = random(1 , 3);
+}
+
+// in addition to setting background color, we are establishing the location of the rectangle on the X axis as a function of its speed; we are also saying that once the rectangle has passed the bounds of the canvas the loop will stop (game over); a message will appear starting at the 100,300 coordinate that announces click Count
+
+function draw () {
+  background('silver');
+  drawShape();
+  rectX += speed;
+  if(rectX > width) {
+    noLoop();
+    textSize(28);
+    textStyle(BOLDITALIC);
+    text('Your score was ' + clickCount + '!', 100, 300);
+}
+}
+
+// this is the shape of the target in the game
+
+function drawShape () {
+    fill('green');
+    rect(rectX, rectY, rectWidth, rectHeight);
+  }
+
+// this is the listening event that "hears" if the user has successfully clicked within the rectangle
+
+function mousePressed () {
+  if ((mouseX >= rectX && mouseX <= rectX + rectWidth) && (mouseY >= rectY && mouseY <= rectY + rectHeight)) {
+      clickCount++;
+    console.log('hit', clickCount);
+  }
+}
