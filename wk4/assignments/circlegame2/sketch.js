ad5df1ad153adf970ec866b213a89d426d8d52ac
Welling Hall <whall@mcad.edu>
2021-02-10 20:06:56 -0500

    fixed with >=

--- wk4/assignments/circlegame2/sketch.js
+++ wk4/assignments/circlegame2/sketch.js
@@ -34,19 +34,18 @@ function draw() { // 1 = draw function
   // if we wanted right side it would be the same but plus x
   const leftSide = x - (ellipseWidth / 2);
   const rightSide = x + (ellipseWidth / 2);
-  console.log('left side', leftSide );
+  console.log('right side', rightSide);
   // the left side of your canvas is 0, the right side is 1000 but you were right when you referred to it as `width`
   // since my circle is now moving right, I'm using greater than (or equal to) width
   // if my circle was moving left to 0, I'd use a less than (or equal to) zero
 
   // you can use (rightSide < 0) to check if the right side was off (the whole circle was off the left side)
-  // if you had a positive xspeed, you would then use (rightSide <= width) - wh - when I make this change to rightSide because I changed from -1 to 1 above, it seems to break again? 
-  if (rightSide <= width) {
+  // if you had a positive xspeed, you would then use (rightSide <= width) - wh - when I make this change to rightSide because I changed from -1 to 1 above, it seems to break again?  Because it needed to be >= :-))))
+  if (rightSide >= width) {
   // announce the count
   noLoop();
-  fill: ('black')
-  textSize(28);
-  textStyle(BOLDITALIC);
+  textSize(42);
+  fill('black');
   text('Your score was ' + clickCount + '!', 100, 300);
 }
 
