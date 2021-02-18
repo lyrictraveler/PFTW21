3ae86ac0fa851c01c2cefbc8353bed1e27cd7574
Welling Hall <whall@mcad.edu>
2021-02-18 11:31:28 -0500

    modifying class bubble sketch

--- wk5/classes/sketch.js
+++ wk5/classes/sketch.js
@@ -1,23 +1,44 @@
 let bubble1;
 let bubble2;
 
+
 function setup() {
   createCanvas(600, 400);
   bubble1 = new Bubble(200, 200, 40);
   bubble2 = new Bubble(400, 200, 20);
+
 }
 
+/* function draw() {
+  background(220);
+  bubble1.move();
+  bubble1.show();
+  bubble2.move();
+  bubble2.show();
+} */
+
+/* function draw() {
+  background(100);
+  
+  for (let circleX = 75; circleX <= 225; circleX += 75) {
+    circle(circleX, 150, 50);
+  }
+} */
 
 function draw() {
-  background(220);
+  background(100);
+  
+  for (let circleX = 75; circleX <= 225; circleX += 75) {
+    circle(circleX, 150, 50);
+  }
 }
 
 class Bubble {
-  constructor(x, y, r) {
+  constructor(x, y) {
     this.x = 200;
     this.y = 150;
-    this.r = r;
   }
+  
   move() {
     this.x = this.x + random(-5, 5);
     this.y = this.y + random(-5, 5)
@@ -26,7 +47,7 @@ class Bubble {
     stroke(255);
     strokeWeight(4);
     noFill();
-    ellipse(this.x, this.y, this.r * 2);
+    ellipse(this.x, this.y, 24, 24);
   }
 
 }
