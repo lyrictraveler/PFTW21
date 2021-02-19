7271a856355b1435744472f49ca7f080301e4744
Welling Hall <whall@mcad.edu>
2021-02-19 18:59:34 -0500

    revised buggy memory card game

--- wk5/flip_card/sketch.js
+++ wk5/flip_card/sketch.js
@@ -1,29 +1,35 @@
-const DOWN = "down";
-const UP = "up";
+const DOWN = 'down';
+const UP = 'up';
 let startingX = 100;
 let startingY = 100;
-let cards = []; // create new array "cards"
-const gameState = {};
+const cards = []; // create new array "cards"
+const gameState = {
+  totalPairs: 6,
+  flippedCards: [],
+  numMatched: 0,
+  attempts: 0,
+  waiting: false
+};
 let cardfaceArray = [];
 let cardBack;
 
-function preload() {
-  cardBack = loadImage("images/cardback.png");
+function preload () {
+  cardBack = loadImage('images/cardback.png');
   cardfaceArray = [
-    loadImage("images/impatiens.jpg"),
-    loadImage("images/jonquil.jpg"),
-    loadImage("images/orchid2.jpg"),
-    loadImage("images/orange.jpg"),
-    loadImage("images/peony.jpg"),
-    loadImage("images/peacerose.jpg"),
+    loadImage('images/impatiens.jpg'),
+    loadImage('images/jonquil.jpg'),
+    loadImage('images/orchid2.jpg'),
+    loadImage('images/orange.jpg'),
+    loadImage('images/peony.jpg'),
+    loadImage('images/peacerose.jpg')
   ];
 }
 
-function setup() {
+function setup () {
   createCanvas(1300, 1500);
-  background(0);
+  //background(0);
   let selectedFaces = [];
-  for (let z = 0; z < 6; z++) {
+  for (let z = 0; z < 5; z++) {
     const randomIdx = floor(random(cardfaceArray.length));
     const face = cardfaceArray[randomIdx];
     selectedFaces.push(face);
@@ -49,46 +55,69 @@ function setup() {
 
 // I think "k" in this next function refers to the "Loop of All Cards", but I'm not sure?
 
-function mousePressed() {
+function mousePressed () {
+  if (gameState.waiting) {
+    return;
+  }
   for (let k = 0; k < cards.length; k++) {
-    if (cards[k].didHit(mouseX, mouseY)) {
-      console.log("flipped", cards[k]);
+    //console.log('gameState', gameState);
+    // first check flipped cards length, and then
+    // we can trigger the flip
+    if (gameState.flippedCards.length < 2 && cards[k].didHit(mouseX, mouseY)) {
+      console.log('flipped', cards[k]);
+      gameState.flippedCards.push(cards[k]);
+      // start at mark 4:22
+    }
+  }
+  if (gameState.flippedCards.length === 2) {
+    gameState.attempts++;
+    if (gameState.flippedCards[0].cardFaceImg === gameState.flippedCards[1].faceImage) {
+      // mark cards as matched so they don't flip back
+      gameState.flippedCards[0].isMatch = true;
+      gameState.flippedCards[1].isMatch = true;
+      // empty flipped cards array
+      gameState.flippedCards.length = 0;
+      // increment the score
+      gameState.numMatched++;
+      loop();
+    } else {
+      gameState.waiting = true;
+      const loopTimeout = window.setTimeout(() => {
+        loop();
+      }, 1000)
+      window.clearTimeout(loopTimeout);
+
     }
   }
 }
 
 // added parameters to constructor
-
 class Card {
-  constructor(x, y, cardFaceImg) {
+  constructor (x, y, cardFaceImg) {
     this.x = x;
     this.y = y;
     this.width = 200;
     this.height = 300;
     this.face = DOWN;
     this.cardFaceImg = cardFaceImg;
+    this.isMatch = false;
     this.show();
   }
 
-  show() {
-    if (this.face === DOWN) {
-      fill("aqua");
-      rect(this.x, this.y, this.width, this.height, 10);
-      image(cardBack, this.x, this.y, 200, 300);
-    } else {
-      fill("aaa");
+  show () {
+    if (this.face === UP || this.isMatch) {
+      fill('gray');
       rect(this.x, this.y, this.width, this.height, 10);
       image(this.cardFaceImg, this.x, this.y, 200, 300);
+    } else {
+      fill('aqua');
+      rect(this.x, this.y, this.width, this.height, 10);
+      image(cardBack, this.x, this.y, 200, 300);
     }
   }
 
-  didHit(mouseX, mouseY) {
-    if (
-      mouseX >= this.x &&
-      mouseX <= this.x + this.width &&
-      mouseY >= this.y &&
-      mouseY <= this.y + this.height
-    ) {
+  didHit (mouseX, mouseY) {
+    if (mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height) {
       this.flip();
       return true;
     } else {
@@ -96,7 +125,7 @@ class Card {
     }
   }
 
-  flip() {
+  flip () {
     if (this.face === DOWN) {
       this.face = UP;
     } else {
@@ -106,7 +135,29 @@ class Card {
   }
 }
 
-function shuffleArray(array) {
+function draw () {
+  background(0);
+  if (gameState.numMatched === gameState.totalPairs) {
+    fill('yellow');
+    textSize(66);
+    text('you win!!!!', 400, 1200);
+    noLoop();
+  }
+  for (let k = 0; k < cards.length; k++) {
+    if(!cards[k].isMatch) {
+      cards[k].face = DOWN;
+    }
+    cards[k].show();
+  }
+  noLoop();
+  gameState.waiting = false;
+  fill(255);
+  textSize(36);
+  text('attempts ' + gameState.attempts, 100, 1100);
+  text('matches ' + gameState.numMatched, 100, 1150);
+}
+
+function shuffleArray (array) {
   let counter = array.length;
   while (counter > 0) {
     // Pick randow index
