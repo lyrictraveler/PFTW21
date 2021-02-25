3cd2c2505299b07f33d280f45554e0ac687b17d5
Welling Hall <whall@mcad.edu>
2021-02-25 10:46:32 -0500

    llama exercise

---
+++ wk6/llama/lib/script.js
@@ -0,0 +1,86 @@
+// Add your code here
+
+new Vue({
+  el: "#title",
+  data: {
+    message: "A Natural History of Llamas",
+  },
+});
+
+new Vue({
+  el: "#fact1",
+  data: {
+    message: "Fact 1: Llama poop does not stink.",
+  },
+});
+
+new Vue({
+  el: "#fact2",
+  data: {
+    message: "Fact 2: The scientific name for llama is llama glama.",
+  },
+});
+
+new Vue({
+  el: "#fact3",
+  data: {
+    message: "Fact 3: Llamas and alpacas are both camelids.",
+  },
+});
+
+new Vue({
+  el: "#fact4",
+  data: {
+    message: "Fact 4: Llamas and alpacas are both mammals.",
+  },
+});
+
+new Vue({
+  el: "#fact5",
+  data: {
+    message: "Fact 5: Llamas and alpacas spit when they are upset.",
+  },
+});
+
+new Vue({
+  el: "#fact6",
+  data: {
+    message: "Fact 6: Llamas and alpacas both live in the Andes.",
+  },
+});
+
+new Vue({
+  el: "#fact7",
+  data: {
+    message: "Fact 7: Llamas and alpacas are not, in fact, the same animal.",
+  },
+});
+
+new Vue({
+  el: "#fact8",
+  data: {
+    message: "Facts 8 - 10: The Llama, a poem by Ogden Nash",
+  },
+});
+
+new Vue({
+  el: "#fact9",
+  data: {
+    message: "The one-l lama / He's a priest.",
+  },
+});
+
+new Vue({
+  el: "#fact10",
+  data: {
+    message: "The two-l llama / He's a beast.",
+  },
+});
+
+new Vue({
+  el: "#fact11",
+  data: {
+    message:
+      "And I will bet / A silk pajama / There isn't any / Three-l lllama",
+  },
+});
