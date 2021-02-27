4e144a00cfe303e718349a3a57a7deefb7292e58
Welling Hall <whall@mcad.edu>
2021-02-27 13:40:07 -0500

    llama2 revised

---
+++ wk6/llama2/lib/script.js
@@ -0,0 +1,24 @@
+// Add your code here
+
+new Vue({
+  el: '#facts',
+  data: {
+    fact1: 'Fact 1: Llama poop does not stink.',
+    fact2: 'Fact 2: The scientific name for llama is llama glama.',
+    fact3: 'Fact 3: Llamas and alpacas are both camelids.',
+    fact4: 'Fact 4: Llamas and alpacas are both mammals.',
+    fact5: 'Fact 5: Llamas and alpacas spit when they are upset.',
+    fact6: 'Fact 6: Llamas and alpacas both live in the Andes.',
+    fact7: 'Fact 7: Llamas and alpacas are not, in fact, the same animal.',
+  }
+})
+
+new Vue({
+  el: '#poetry',
+  data: {
+    fact8: "The one-l lama / He's a priest.",
+    fact9: "The two-l llama / He's a beast.",
+    fact10:  "And I will bet / A silk pajama / There isn't any / Three-l lllama",
+    poetryIsAwesome: true, 
+  }
+})
