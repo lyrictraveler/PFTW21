fdbba6a8d21ac037ea9a71489bb91f79ab6118eb
Welling Hall <whall@mcad.edu>
2021-03-02 09:32:21 -0500

    prettify

--- wk7/assign7_1/assign7_1.js
+++ wk7/assign7_1/assign7_1.js
@@ -1,23 +1,6 @@
-/* eslint-disable no-undef */
-/* //This is inline style
 var vm = new Vue({
   el: "#app",
   data: {
-    baseStyle: {
-      background: "red",
-      border: "1px solid black",
-    },
-    overRideStyle: {
-      background: "purple",
-      padding: "20px",
-      margin: "0 20px"
-    }
-  }
-});
- */
-var vm = new Vue({
-  el: "#app",
-  data: {
-    currentRoute: 'features'
-  }
+    currentRoute: "features",
+  },
 });
