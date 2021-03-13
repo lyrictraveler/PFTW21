4d3f313653b7cd55e614e40ab4d8cfb020a6d3da
ken-teaches-web <kkorth@mcad.edu>
2021-03-13 10:25:47 -0600

    fix: put form inside doll-list app, added data props for form

--- wk8/matreoshki/lib/script.js
+++ wk8/matreoshki/lib/script.js
@@ -1,61 +1,68 @@
 const russianDollsArray = [
-          {
-            image: "img/sovietdoll.jpg",
-            name: "Soviet",
-            acquired: 1984,
-            count: 9,
-            isArtisan: false,
-            description: "Nesting dolls mass-produced in the USSR for tourist purchases in hard currency. Poor quality stencil design in primary colors.",
-            id: 1
-          },
+  {
+    image: "img/sovietdoll.jpg",
+    name: "Soviet",
+    acquired: 1984,
+    count: 9,
+    isArtisan: false,
+    description: "Nesting dolls mass-produced in the USSR for tourist purchases in hard currency. Poor quality stencil design in primary colors.",
+    id: 1
+  },
 
-          {
-            image: "img/oniondomedoll.jpg",
-            name: "Blue Nun",
-            acquired: 1989,
-            count: 5,
-            isArtisan: true,
-            description: "Early glasnost' era hand-painted monochromatic nesting dolls featuring Russian Orthodox onion-dome church on each doll's apron.",
-            id: 2
-          },
-          {
-            image: "img/malevichdoll.jpg",
-            name: "Malevich",
-            acquired: 1990,
-            count: 5,
-            isArtisan: true,
-            description: "Museum-quality post-Soviet hand-painted nesting dolls in style of avant-garde artist Kazimir Malevich.",
-            id: 3
-          }, 
-          {
-            image: "img/fairytaledoll.jpg",
-            name: "Firebird",
-            acquired: 1994,
-            count: 5,
-            isArtisan: true,
-            description: "Intricately hand-decorated nesting dolls decorated with gold leaf and representing scenes from a traditional Russian folk tale.",
-            id: 4
-          }, 
-          {
-            image: "img/putindoll.jpg",
-            name: "Putin",
-            acquired: 2000,
-            count: 10,
-            isArtisan: false,
-            description: "Mass-produced in post-Soviet factory for tourist consumption. Large example in genre of political nesting dolls. From largest to smallest, dolls represent Putin, Yeltsin, Gorbachev, Brezhnev, Khrushchev, Stalin, Lenin, Marx, Engels, and Ivan the Terrible.",
-            id: 5
-          }, 
-        ];
+  {
+    image: "img/oniondomedoll.jpg",
+    name: "Blue Nun",
+    acquired: 1989,
+    count: 5,
+    isArtisan: true,
+    description: "Early glasnost' era hand-painted monochromatic nesting dolls featuring Russian Orthodox onion-dome church on each doll's apron.",
+    id: 2
+  },
+  {
+    image: "img/malevichdoll.jpg",
+    name: "Malevich",
+    acquired: 1990,
+    count: 5,
+    isArtisan: true,
+    description: "Museum-quality post-Soviet hand-painted nesting dolls in style of avant-garde artist Kazimir Malevich.",
+    id: 3
+  },
+  {
+    image: "img/fairytaledoll.jpg",
+    name: "Firebird",
+    acquired: 1994,
+    count: 5,
+    isArtisan: true,
+    description: "Intricately hand-decorated nesting dolls decorated with gold leaf and representing scenes from a traditional Russian folk tale.",
+    id: 4
+  },
+  {
+    image: "img/putindoll.jpg",
+    name: "Putin",
+    acquired: 2000,
+    count: 10,
+    isArtisan: false,
+    description: "Mass-produced in post-Soviet factory for tourist consumption. Large example in genre of political nesting dolls. From largest to smallest, dolls represent Putin, Yeltsin, Gorbachev, Brezhnev, Khrushchev, Stalin, Lenin, Marx, Engels, and Ivan the Terrible.",
+    id: 5
+  },
+];
 
-  var vm = new Vue({
+var vm = new Vue({
   el: "#dolls-list",
   data: {
-    dolls: russianDollsArray
+    dolls: russianDollsArray,
+    imageInput: '',
+    nameInput: '',
+    acquiredInput: '',
+    isArtisan: '',
+    descriptionInput: '',
+    countInput: '',
+    idInput: ''
   },
   methods: {
     deleteDoll: function (dollObject) {
       console.log("doll", dollObject)
-      this.dolls = this.dolls.filter(function(doll){
+      this.dolls = this.dolls.filter(function (doll) {
         if (doll.id !== dollObject.id) {
           return true;
         } else {
@@ -63,7 +70,8 @@ const russianDollsArray = [
         }
       })
     },
-    addDoll: function () {
+    addDoll: function (e) {
+      e.preventDefault();
       let newDoll = {
         image: this.imageInput,
         name: this.nameInput,
