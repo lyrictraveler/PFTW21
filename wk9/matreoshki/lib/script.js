5b2843383e499a7a6a56461068e5e1e3e66b64fc
Welling Hall <whall@mcad.edu>
2021-03-20 13:34:59 -0400

    turning rows into components

---
+++ wk9/matreoshki/lib/script.js
@@ -0,0 +1,97 @@
+// eslint-disable-next-line no-unused-expressions
+Vue.component('matreoshka-component', {
+  template: `<tr class="dolls-container" v-cloak>
+              <td class="cell"><img v-bind:src="image" class="photo"></td>
+              <td class="cell">{{name}}</td>
+              <td class="cell">{{acquired}}</td>
+              <td class="cell">{{count}}</td>
+              <td class="cell__description">{{description}}</td>
+              <td></td>
+            </tr>`,
+  props: ['image', 'name', 'acquired', 'count', 'description']
+});
+
+const russianDollsArray = [
+  {
+    image: 'img/sovietdoll.jpg',
+    name: 'Soviet',
+    acquired: 1984,
+    count: 9,
+    isArtisan: false,
+    description:
+      'Nesting dolls mass-produced in the USSR for tourist purchases in hard currency. Poor quality stencil design in primary colors.',
+    id: 1
+  },
+
+  {
+    image: 'img/oniondomedoll.jpg',
+    name: 'Blue Nun',
+    acquired: 1989,
+    count: 5,
+    isArtisan: true,
+    description:
+      "Early glasnost' era hand-painted monochromatic nesting dolls featuring Russian Orthodox onion-dome church on each doll's apron.",
+    id: 2
+  },
+  {
+    image: 'img/malevichdoll.jpg',
+    name: 'Malevich',
+    acquired: 1990,
+    count: 5,
+    isArtisan: true,
+    description:
+      'Museum-quality post-Soviet hand-painted nesting dolls in style of avant-garde artist Kazimir Malevich.',
+    id: 3
+  },
+  {
+    image: 'img/fairytaledoll.jpg',
+    name: 'Firebird',
+    acquired: 1994,
+    count: 5,
+    isArtisan: true,
+    description:
+      'Intricately hand-decorated nesting dolls decorated with gold leaf and representing scenes from a traditional Russian folk tale.',
+    id: 4
+  },
+  {
+    image: 'img/putindoll.jpg',
+    name: 'Putin',
+    acquired: 2000,
+    count: 10,
+    isArtisan: false,
+    description:
+      'Mass-produced in post-Soviet factory for tourist consumption. Large example in genre of political nesting dolls. From largest to smallest, dolls represent Putin, Yeltsin, Gorbachev, Brezhnev, Khrushchev, Stalin, Lenin, Marx, Engels, and Ivan the Terrible.',
+    id: 5
+  }
+];
+
+var vm = new Vue({
+  el: '#dolls-list',
+  data: {
+    dolls: russianDollsArray,
+    imageInput: '',
+    nameInput: '',
+    acquiredInput: '',
+    isArtisan: '',
+    descriptionInput: '',
+    countInput: '',
+    idInput: ''
+  },
+  methods: {
+
+    addDoll: function (e) {
+      e.preventDefault();
+      const newDoll = {
+        image: this.imageInput,
+        name: this.nameInput,
+        acquired: this.acquiredInput,
+        count: this.countInput,
+        isArtisan: this.isArtisanInput,
+        description: this.descriptionInput,
+        id: this.idInput
+      };
+      this.dolls.push(newDoll);
+      this.typeInput = this.locationInput = this.dateInput = this.idInput = '';
+    }
+  }
+});
