3aa0a04339d5d42fede51317fd7def91caf45695
Welling Hall <whall@mcad.edu>
2021-03-28 22:54:04 -0400

    formatted

--- wk10/music/music.js
+++ wk10/music/music.js
@@ -1,39 +1,35 @@
-
-Vue.component('streaming-track', {
-    template: `<div class="track">
+Vue.component("streaming-track", {
+  template: `<div class="track">
         <h3>{{track.title}}</h3>
         <div><img v-bind:src="track.cover" alt=""></div>
         <small>{{track.artist}}</small>
         </div>`,
-    props: ['track'],
+  props: ["track"],
 
-    computed: {
-        trending: function () {
-            const delta = this.track.rank - this.track.position.positionLastWeek;
-            if (delta > 0) {
-                return 'up' 
-            } else if (delta < 0) {
-                return 'down' 
-            } else {
-                return 'no-change'
-            }
-        }
-    }
+  computed: {
+    trending: function () {
+      const delta = this.track.rank - this.track.position.positionLastWeek;
+      if (delta > 0) {
+        return "up";
+      } else if (delta < 0) {
+        return "down";
+      } else {
+        return "no-change";
+      }
+    },
+  },
 });
 
 const vm = new Vue({
-    el: "#musicApp",
-    data: {
-        tracks: []
-    },
-    mounted () {
-        axios
-            .get('./data/music-list.json')
-            .then(response => {
-                console.log('response', response);
-                vm.tracks = response.data;
-                console.log(vm.tracks);
-            });
-
-    }
-})
+  el: "#musicApp",
+  data: {
+    tracks: [],
+  },
+  mounted() {
+    axios.get("./data/music-list.json").then((response) => {
+      console.log("response", response);
+      vm.tracks = response.data;
+      console.log(vm.tracks);
+    });
+  },
+});
