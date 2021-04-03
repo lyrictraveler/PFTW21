89d9fea81b27aef83f3f989980f7a7506815f65f
Welling Hall <whall@mcad.edu>
2021-04-03 14:40:14 -0400

    refined CSS

--- wk10/art/art.js
+++ wk10/art/art.js
@@ -1,17 +1,19 @@
 Vue.component('met-departments', {
-  template: `<div :class="['department']">
-        <h3>{{department.displayName}}</h3>
-        <small>{{department.departmentId}}</small>
+  template: `<div class="department">
+        <h3>{{displayName}}</h3>
+        <small>{{departmentId}}</small>
         </div>`,
   props: ['departmentId', 'displayName']
 });
 
-const vm = new Vue({
+const vm = new Vue ({
   el: '#artApp',
   data: {
-    departments: []
+    departments: [],
+    foo:"bar"
   },
-  mounted () {
+  mounted:function () {
+    console.log("mounted")
     axios.get('./data/met-departments.json').then((response) => {
       console.log('response', response);
       vm.departments = response.data;
