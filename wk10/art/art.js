96122d917f7cd178fc08ce76d1551a21de9bfdb9
Welling Hall <whall@mcad.edu>
2021-04-03 14:48:08 -0400

    prettified docs

--- wk10/art/art.js
+++ wk10/art/art.js
@@ -1,23 +1,23 @@
-Vue.component('met-departments', {
+Vue.component("met-departments", {
   template: `<div class="department">
         <h3>{{displayName}}</h3>
         <small>{{departmentId}}</small>
         </div>`,
-  props: ['departmentId', 'displayName']
+  props: ["departmentId", "displayName"],
 });
 
-const vm = new Vue ({
-  el: '#artApp',
+const vm = new Vue({
+  el: "#artApp",
   data: {
     departments: [],
-    foo:"bar"
+    foo: "bar",
   },
-  mounted:function () {
-    console.log("mounted")
-    axios.get('./data/met-departments.json').then((response) => {
-      console.log('response', response);
+  mounted: function () {
+    console.log("mounted");
+    axios.get("./data/met-departments.json").then((response) => {
+      console.log("response", response);
       vm.departments = response.data;
       console.log(vm.departments);
     });
-  }
-})
+  },
+});
