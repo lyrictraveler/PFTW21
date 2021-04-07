a56a69ab12ff9330574bf7e21ae8509e1229ea4e
Welling Hall <whall@mcad.edu>
2021-04-07 09:53:13 -0400

    edits

--- wk11/russianDolls/sketch.js
+++ wk11/russianDolls/sketch.js
@@ -1,33 +1,40 @@
-// I looked at this site to try to randomize the angle and the transparency of the dolls in a loop, but I couldn't get it to work <https://forum.processing.org/two/discussion/21006/how-to-give-random-colour-and-transparency-to-an-object-class-from-an-array-in-the-object>
+// I looked at this site to try to randomize the angle of the dolls in a loop, but I couldn't get it to work <https://forum.processing.org/two/discussion/21006/how-to-give-random-colour-and-transparency-to-an-object-class-from-an-array-in-the-object>
 
 let rot_angle // This doesn't work; I don't know why. No error messages. 
 
+var r;
+var g;
+var b;
+var a;
+
 function setup() {
   createCanvas(480, 120);
+  randomColor = color(random(255), random(255), random(255));
+  randomColor2 = color(random(255), random(255), random(255));
 }
 
 function draw() {
   background("silver");
+
   randomSeed(0);
   for (var i = 35; i < width + 40; i += 40) {
-      if (i%5==0) {
-      rot_angle=20;
+    if (i % 5 == 0) {
+      rot_angle = 20;
     } else {
-      rot_angle=0;
+      rot_angle = 0;
     }
-      var scalar = random(0.25, 1.5);
-      /*var transparency = random(0, 255); -- would love to know why this doesn't work */
-      nestingdoll(i, 110, scalar);
+    var scalar = random(0.25, 1.5);
+    nestingdoll(i, 110, scalar);
   }
 }
+
 
 //The nestingdoll is derived from the owl function found in our Make text
 function nestingdoll(x, y, s) {
   push();
   translate(x, y);
   scale(s); // Set the scale
-  /*stroke(t); // Set the transparency*/
-  stroke("blue");
+  stroke(randomColor);
   strokeWeight(70);
   line(0, -35, 0, -65); // Body
   noStroke();
@@ -36,11 +43,11 @@ function nestingdoll(x, y, s) {
   fill(0);
   ellipse(-6, -70, 4, 4); // Left eye
   ellipse(6, -70, 4, 4); // Right eye
-  fill("fuchsia");
+  fill("red");
   ellipse(0, -60, 8, 8); // Mouth
-  fill("yellow"); 
-  ellipse(0, -25, 40, 40); // Apron
-  stroke("yellow");
+  fill(randomColor2); // Apron
+  ellipse(0, -25, 40, 40);
+  stroke(randomColor2);
   strokeWeight(5);
   line(-35, -35, 35, -35);
   pop();
