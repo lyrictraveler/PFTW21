b2d8a3f5917cc49e86f35af53903ce35eeb367ce
Welling Hall <whall@mcad.edu>
2021-05-04 10:06:05 -0400

    add components to beforeafter page

---
+++ wk14/portfoliocards/lib/beforeafter.js
@@ -0,0 +1,48 @@
+// instructions:
+
+// convert existing Vue instance into a reusable component
+
+//register the component using kabob-case so browser can recognize
+
+Vue.component('beforeafter-component', {
+  // templates are written in HTML form, a "configuration object" With multi-line HTML use the back tick `
+
+  template: `<div class="portfolio-container v-cloak">
+                <h2 class="project-title">{{title}}</h2>
+                <div class="image">
+                  <img v-bind:src="image" v-bind:alt="image-alt"/>
+                </div>
+                <div class="content">
+            </div>`,
+
+  props: ['title', 'image', 'imageAlt']
+});
+
+// Your component should go above this line.
+var vm = new Vue({
+  el: '#card',
+  data: {
+    items: [
+      {
+        title: 'Before',
+        image: 'img/nor_misery.jpeg',
+        imageAlt: 'woman with head in her hands'
+      },
+      {
+        title: 'After',
+        image: 'img/nor_hppiness.jpeg',
+        imageAlt: 'curious woman'
+      },
+      {
+        title: 'Before',
+        image: 'img/jeff_shock.jpeg',
+        imageAlt: 'shocked man'
+      },
+      {
+        title: 'After',
+        image: 'img/jeff_happy.jpeg',
+        imageAlt: 'man content'
+      },
+    ],
+  },
+});
